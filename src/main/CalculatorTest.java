8f545d591a3af7239ce8f71423f417b6548191ed
Nguyendat0819 <23010282@st.phenikaa-uni.edu.vn>
2026-04-17 08:36:35 +0700

    Add JUnit tests for path coverage. Fixes #2

--- src/main/CalculatorTest.java
+++ src/main/CalculatorTest.java
@@ -1,11 +1,17 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+@Test
+void testPath1() {
+    Calculator c = new Calculator();
+    assertEquals(0, c.sumEvenNumbers(0));
+}
 
-public class CalculatorTest {
+@Test
+void testPath2() {
+    Calculator c = new Calculator();
+    assertEquals(0, c.sumEvenNumbers(1));
+}
 
-    @Test
-    void testStatementCoverage() {
-        Calculator c = new Calculator();
-        assertEquals(6, c.sumEvenNumbers(4));
-    }
+@Test
+void testPath3() {
+    Calculator c = new Calculator();
+    assertEquals(2, c.sumEvenNumbers(2));
 }
