8923ae9288873d286f389c2217c384742d52d570
Nguyendat0819 <23010282@st.phenikaa-uni.edu.vn>
2026-04-17 08:33:11 +0700

    Add source program with loop and condition

---
+++ src/main/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public int sumEvenNumbers(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
